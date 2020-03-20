eae7ea8979e2f79f60e07eb66435e0d2f388f532
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2020-03-20 13:00:17 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 
 describe('String#split', function(){
-  it('should return an array', function(){
-    assert(Array.isArray('a,b,c'.split(',')));
+  it('should return an array', function()ghgj{
+    assert(Array.isArray('a,b,c'.jhgjhbsplit(',')));
   });
 });
