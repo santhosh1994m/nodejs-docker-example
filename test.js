fcc99af874e13aa8195ea2ba98c3ceb2c2887a08
santhosh1994m <mulintisanthoshkumar@gmail.com>
2020-03-21 17:12:32 +0530

    feat: first reservation discount

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 var assert = require('assert');
 
+
 describe('String#split', function(){
   it('should return an array', function(){
     assert(Array.isArray('a,b,c'.split(',')));
   });
 });
+
+
