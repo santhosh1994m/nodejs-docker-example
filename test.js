c0569c83305ae52c040eb5dd2d65772fef7729c0
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2020-03-27 00:36:25 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,36 +1,10 @@
 var assert = require('assert');
-vrnjiernvervv
-evrnevnr
-meorvnelrkv
-movneornv
-mornveornv
-mvoeinrvoenrvlemwlnfo3
-wmoiwnowmelwmk
-mvornvowmev
-wmoernpwmec;wmvoi3n4ovl vwwvvmeprvmoernvoenvev
-evmoejnvoenvpm
-venvonbon
-wvm[orvneornvoenv
-    wmoenvurbojwonongon4uhgiu4hginsldmgb4
-    wgpingonsrgnengelfl;kegheonm
-    ajgoengeonsflkmignwlf[ngenfw
-    gnepnfofelkneou3nfknwoufjs dme gkjrnod
-    wnfienrfonfkenfone,3
-    wfeoienrvfoi4ngnesfo
-    nogjio4jg4gp,lngonkbon3w
-    5gniungofm'3guonpl]
-
+]
 describe('String#split', function(){
   it('should return an array', function(){
     assert(Array.isArray('a,b,c'.split(',')));
   });
 });
 
-cefnrhef
-erviebriebrv
-erneorneirnv
-rvinerion
 
-if :
-    
-    if ()lce;
+if c
